c6a4c8d9c6e58ae0a6cbf5b1c47f0078d39a5fae
Hitesh <Hk2950@nyu.edu>
2019-11-09 13:42:00 -0500

    fixed app.py

--- app.py
+++ app.py
@@ -206,8 +206,8 @@ def login():
     if request.method == 'POST':
         enc_pass = bcrypt.generate_password_hash(request.form['password'])        
         if enc_pass is None:
-            flash('Username or Password is invalid' , 'error')
-            return redirect(url_for('login')
+        	flash('Username or Password is invalid' , 'error')
+        	return redirect(url_for('login'))
         inputuser=request.form['username']
         myuser = db.session.query(Userinfo).filter(Userinfo.myusername==request.form['username']).first()
         enc_pass_a = bcrypt.check_password_hash(enc_pass, myuser.mypassword)
